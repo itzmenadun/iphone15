d4da24abc7b7e72baa36eca8ad51735bfd284fc1
Nadun Sumedha <90174385+NadunKMS@users.noreply.github.com>
2024-07-03 16:43:24 +0530

    Initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,62 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { heroVideo, smallHeroVideo } from '../utils'
+import { useEffect, useState } from "react"
+
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo)
+    } else {
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect( () => {
+    window.addEventListener('resize', handleVideoSrcSet)
+
+    return () => {
+      window.removeEventListener('resize',handleVideoSrcSet)
+    }
+  },[])
+
+  useGSAP(() => {
+    gsap.to('#hero', { 
+      opacity: 1,
+      delay: 2
+    })
+
+    gsap.to('#cta', {
+      opacity: 1,
+      y: -50,
+      delay: 2
+    })
+  },[])
+
+  return (
+    <section className="relative w-full bg-black nav-height">
+
+      <div className="flex-col w-full h-5/6 flex-center">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
+
+      <div 
+      id="cta"
+      className="flex flex-col items-center translate-y-20 opacity-0">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="text-sm font-normal text-gray-200">From Rs.49,999/month or Rs.199,999</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+//import React from 'react'
+import { appleImg, searchImg, bagImg } from '../utils'
+import { navLists} from '../constants'
+
+const Navbar = () => {
+  return (
+    <header className='flex items-center justify-between w-full px-5 py-5 sm:px-10'>
+    <nav className='flex w-full screen-max-width'>
+      <img src={appleImg} alt="Apple logo" width={18} height={22} />
+
+      <div className='flex items-center justify-center flex-1 max-sm:hidden'>
+        {navLists.map((nav) => (
+          <div key={nav} className='px-5 text-sm transition-all cursor-pointer text-gray hover:text-white'>
+            {nav}
+          </div>
+          ))}
+      </div>
+
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+
+    </nav>
+  </header>
+  )
+}
+
+export default Navbar
